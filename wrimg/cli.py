3ea388fb74229e0731e238713637a99e416a6aea
Marc Brinkmann <git@marcbrinkmann.de>
2015-06-26 17:04:50 +0200

    Most of the safety checks are in place.

--- wrimg/cli.py
+++ wrimg/cli.py
@@ -1,4 +1,5 @@
 from functools import partial
+import sys
 
 import click
 
@@ -32,6 +33,10 @@ def candidate_for_writing(dev):
     return True, None
 
 
+def dev_info(dev):
+    return '{0.path} ({0.size:.1fH}): {0.model}'.format(dev)
+
+
 @click.command()
 @click.argument('image-file')
 @click.option('--target', '-t',
@@ -39,7 +44,8 @@ def candidate_for_writing(dev):
               help='The target to write to. If none is given, a menu is shown'
                    ' to select one.')
 @click.option('--verbose', '-v', is_flag=True, default=False)
-def wrimg(image_file, target, verbose):
+@click.option('--i-know-what-im-doing', is_flag=True, default=False)
+def wrimg(image_file, target, verbose, i_know_what_im_doing):
     if verbose:
         info = click.echo
     else:
@@ -59,18 +65,38 @@ def wrimg(image_file, target, verbose):
         if not candidates:
             error('No device given on command-line and no suitable candidate '
                   'found')
-            return 1
+            sys.exit(1)
+        elif len(candidates) == 1:
+            target = candidates[0]
+        else:
+            # display menu
+            for i, c in enumerate(candidates):
+                click.echo('[{}] {}'.format(i, dev_info(c)))
 
-        # display menu
-        for i, c in enumerate(candidates):
-            click.echo(
-                '[{1}] {0.path} ({0.size:.1fH}): {0.model}'
-                .format(c, i))
-        idx = int(click.prompt(
-            'Select a device',
-            type=click.Choice(map(str, range(len(candidates))))
-        ))
+            idx = int(click.prompt(
+                'Select a device',
+                type=click.Choice(map(str, range(len(candidates))))
+            ))
 
-        target = candidates[idx]
+            target = candidates[idx]
+    else:
+        target = Device(target)
 
-    info('target device: {}'.format(target))
+    # sanity-check target-device
+    ok, msg = candidate_for_writing(target)
+
+    if not ok:
+        if not i_know_what_im_doing:
+            error('{.path}: {}.\nAdd --i-know-what-im-doing to disable this '
+                  'check.'
+                  .format(target, msg))
+            sys.exit(0)
+        error('WARNING: {}: {}'.format(dev_info(target), msg))
+
+    # confirm start, featuring the defensive assert!
+    assert click.confirm('Write to {}?'.format(dev_info(target)),
+                         err=True, abort=True)
+
+    info('Writing {} to {.path}'.format(image_file, target))
+
+    # transfer begins here
