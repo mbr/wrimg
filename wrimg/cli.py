415d605fc4986744af100488fde8177d5b7c9a41
Marc Brinkmann <git@marcbrinkmann.de>
2015-10-09 17:02:35 +0200

    Added ability to extract images on the fly.

--- wrimg/cli.py
+++ wrimg/cli.py
@@ -1,6 +1,7 @@
 from functools import partial
 import os
 from stat import S_IFREG
+import subprocess
 import sys
 import time
 
@@ -41,6 +42,51 @@ def dev_info(dev):
     return '{0.path} ({0.size:.1fH}): {0.model}'.format(dev)
 
 
+class Reader(object):
+    # FIXME: replace with reader that encompasses complete pipe and can
+    #        count bytes in as well as bytes out, for more accurate
+    #        progressbars
+    BUFFER_SIZE_MIN = 1024             # 1 KB
+    BUFFER_SIZE_MAX = 16 * 1024 * 1024  # 16 M
+    BUFFER_SIZE_DEFAULT = 512 * 1024   # 512 KB
+
+    def __init__(self, src, buffer_size, limit=None):
+        self.src = src
+        self.bytes_read = 0
+        self.buffer_size = buffer_size or self.BUFFER_SIZE_DEFAULT
+        assert self.BUFFER_SIZE_MIN <= self.buffer_size <= self.BUFFER_SIZE_MAX
+        self.limit = limit
+
+    def increase_buffer(self):
+        self.buffer_size = min(self.buffer_size * 2, self.BUFFER_SIZE_MAX)
+
+    def decreate_buffer(self):
+        self.buffer_size = max(self.buffer_size // 2, self.BUFFER_SIZE_MIN)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.limit is not None:
+            read_size = min(self.buffer_size, self.limit - self.bytes_read)
+        else:
+            read_size = self.buffer_size
+
+        if read_size <= 0:
+            raise StopIteration
+
+        buf = self.src.read(read_size)
+
+        if not buf:
+            raise StopIteration
+
+        self.bytes_read += len(buf)
+
+        return buf
+
+    next = __next__
+
+
 @click.command()
 @click.argument('image-file', type=click.Path(readable=True, dir_okay=False,
                                               exists=True))
@@ -53,8 +99,13 @@ def dev_info(dev):
 @click.option('--i-know-what-im-doing', is_flag=True, default=False)
 @click.option('--chunk-size', '-C', type=ByteSize, default=None,
               help='Read-buffer size (default:Lauto-adjust)')
+@click.option('-x', '--extract', is_flag=True,
+              help='Transparently decompress image while writing')
+@click.option('-d', '--compression-type', default='auto',
+              type=click.Choice(['auto', 'xz']),
+              help='Compression-type')
 def wrimg(image_file, target, verbose, i_know_what_im_doing, limit,
-          chunk_size):
+          chunk_size, extract, compression_type):
     if verbose:
         info = click.echo
     else:
@@ -102,57 +153,90 @@ def wrimg(image_file, target, verbose, i_know_what_im_doing, limit,
             sys.exit(0)
         error('WARNING: {}: {}'.format(dev_info(target), msg))
 
+    # determine compression type
+    verb = 'Write'
+    if extract:
+        if compression_type == 'auto':
+            if image_file.endswith('.xz'):
+                compression_type = 'xz'
+            else:
+                error('Could not determine compression type from file-ending')
+                sys.exit(1)
+
+        info('Decompression enabled, using {}'.format(compression_type))
+        verb = 'Decompress ({}) and write'.format(compression_type)
+
     # confirm start, featuring the defensive assert!
-    assert click.confirm('Write to {}?'.format(dev_info(target)),
+    assert click.confirm('{} to {}?'.format(verb, dev_info(target)),
                          err=True, abort=True)
 
     # determine number of bytes to write
     img_st = os.stat(image_file)
 
-    # start with the device size
+    # start with the target device size
     total = target.size
 
-    if img_st.st_mode & S_IFREG:
+    if not extract and img_st.st_mode & S_IFREG:
         # regular file, limit by its size
         total = min(total, img_st.st_size)
 
-    # apply limt
+    # apply limit
     if limit is not None:
         total = min(total, limit)
 
     info('Copying {:.0f} bytes from {} to {.path}'.format(
          total, image_file, target))
 
-    # start with 4K, go up to 4M with bufsize
-    MIN_BUFSIZE = 4 * 1024
-    MAX_BUFSIZE = 1024 * MIN_BUFSIZE
-    bufsize = chunk_size or MIN_BUFSIZE
+    with open(image_file, 'rb') as src, target.open('wb') as dst:
+        # compression hooks in here
+        if extract:
+            if compression_type == 'xz':
+                decompress = subprocess.Popen(
+                    ['xz', '--decompress', '--stdout'],
+                    stdout=subprocess.PIPE,
+                    stdin=src,
+                )
+            else:
+                raise NotImplementedError('Unknown compressoin type {}'
+                                          .format(compression_type))
 
-    with click.progressbar(length=total, label='writing', info_sep=' | ')\
-            as bar, open(image_file, 'rb') as src, target.open('wb') as dst:
-        while total:
-            # measure time
-            start = time.time()
+            src = decompress.stdout
 
-            buf = src.read(min(total, bufsize))
-            if not buf:
-                break
-            dst.write(buf)
-            dst.flush()
-            os.fsync(dst.fileno())
+        reader = Reader(src, chunk_size, limit)
 
-            end = time.time()
+        if not extract:
+            pbar = click.progressbar(length=total, label='writing',
+                                     info_sep='| ')
+        else:
+            def show_bytes_written(item):
+                return 'total: {:.1f H}'.format(ByteSize(reader.bytes_read))
+            pbar = click.progressbar(reader, label='writing', info_sep='| ',
+                                     item_show_func=show_bytes_written)
 
-            speed = ByteSize(len(buf) / (end-start))
+        with pbar as bar:
+            chunk_source = pbar if extract else reader
 
-            # adjust chunk size if needed
-            if chunk_size is None:
-                if end - start > 2 and bufsize > MIN_BUFSIZE:
-                    # took longer then two seconds, halve bufsize
-                    bufsize //= 2
-                elif end - start < 0.5 and bufsize < MAX_BUFSIZE:
-                    bufsize *= 2
+            for chunk in chunk_source:
+                # measure time
+                start = time.time()
 
-            total -= len(buf)
-            bar.label = '{:.1f H}/s'.format(speed)
-            bar.update(len(buf))
+                dst.write(chunk)
+                dst.flush()
+                os.fsync(dst.fileno())
+
+                end = time.time()
+
+                speed = ByteSize(len(chunk) / (end-start))
+
+                # adjust chunk size if needed
+                if chunk_size is None:
+                    if end - start > 2:
+                        # took longer then two seconds, halve bufsize
+                        reader.decreate_buffer()
+                    elif end - start < 0.5:
+                        reader.increase_buffer()
+
+                bar.label = '{:.1f H}/s'.format(speed)
+
+                if not extract:
+                    bar.update(len(chunk))
