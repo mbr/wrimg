6d9893d6348992d434a15c669a7627b28376a199
Marc Brinkmann <git@marcbrinkmann.de>
2016-04-04 11:54:24 +0200

    Support python 3, YapF setup.py.

--- setup.py
+++ setup.py
@@ -10,25 +10,22 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-setup(
-    name='wrimg',
-    version='0.2.dev1',
-    description='An alternative to dd for writing images to sd-cards',
-    long_description=read('README.rst'),
-    author='Marc Brinkmann',
-    author_email='git@marcbrinkmann.de',
-    url='http://github.com/mbr/wrimg',
-    license='MIT',
-    packages=find_packages(exclude=['tests']),
-    install_requires=['click'],
-    entry_points={
-        'console_scripts': [
-            'wrimg = wrimg.cli:wrimg',
-        ],
-    },
-    classifiers=[
-        'Programming Language :: Python :: 2',
-        # python3 support should be possible, but currently untested
-        #'Programming Language :: Python :: 3',
-    ]
-)
+setup(name='wrimg',
+      version='0.2.dev1',
+      description='An alternative to dd for writing images to sd-cards',
+      long_description=read('README.rst'),
+      author='Marc Brinkmann',
+      author_email='git@marcbrinkmann.de',
+      url='http://github.com/mbr/wrimg',
+      license='MIT',
+      packages=find_packages(exclude=['tests']),
+      install_requires=['click'],
+      entry_points={
+          'console_scripts': [
+              'wrimg = wrimg.cli:wrimg',
+          ],
+      },
+      classifiers=[
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+      ])
