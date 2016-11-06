63e883b48115fb0cd8bd53be22f9070c83e6986f
Marc Brinkmann <git@marcbrinkmann.de>
2016-11-06 17:16:22 +0100

    Added notification once process is complete.

--- wrimg/cli.py
+++ wrimg/cli.py
@@ -285,3 +285,4 @@ def wrimg(image_file, target, verbose, i_know_what_im_doing, limit, chunk_size,
     # we're done writing, call eject
     info('Ejecting {}'.format(target.path))
     subprocess.check_call(['eject', target.path])
+    click.echo('Safe to remove {}'.format(target.path))
